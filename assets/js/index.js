464779b8e12ddd87590e70fbf8f9b5a4800f157d
Virgilio <vbaez@bluecorona.com>
2022-07-03 16:26:02 -0400

    more changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,53 @@
-const list = document.querySelector(".list-icon");
-const grid = document.querySelector(".grid-icon");
+const listIcon = document.querySelector(".list-icon");
+const gridIcon = document.querySelector(".grid-icon");
 const artistsContainer = document.querySelector(".artists-container");
 
 
-list.addEventListener("click", function() {
+listIcon.addEventListener("click", function() {
     artistsContainer.classList.add("list");
-    grid.classList.remove("active");
-    list.classList.add("active");
+    gridIcon.classList.remove("active");
+    listIcon.classList.add("active");
 });
 
-grid.addEventListener("click", function() {
+gridIcon.addEventListener("click", function() {
     artistsContainer.classList.remove("list");
-    list.classList.remove("active");
-    grid.classList.add("active");
+    listIcon.classList.remove("active");
+    gridIcon.classList.add("active");
 });
+
+
+// function resize() {
+//     if ( ($(window).innerWidth() < 1199) && (!(artistsContainer.classList.contains(".list"))) ) {
+//         if (!gridIcon.classList.contains(".active")) {
+//             gridIcon.classList.remove("active");
+//             listIcon.classList.add("active");
+//         }
+//         artistsContainer.classList.add("list");
+
+//     } else if (($(window).innerWidth() >= 1200) && (!(artistsContainer.classList.contains(".list")))) {
+//         if (!listIcon.classList.contains(".active")) {
+//             listIcon.classList.remove("active");
+//             gridIcon.classList.add("active");
+//         }
+//         artistsContainer.classList.remove("list");
+//     }
+// }
+
+// function resize() {
+//     if ( ($(window).innerWidth() < 1200) ) {
+//         if (!artistsContainer.classList.contains("force-list")) {
+//             artistsContainer.classList.add("force-list");
+//         } else {
+//             artistsContainer.classList.remove("force-list");
+//         }
+//     } 
+// }
+
+$(window).bind("resize", function () {
+//    console.log($(this).width());
+    if ($(this).width() < 1200) {
+        $(".artists-container").addClass("force-list"); 
+    } else {
+        $(".artists-container").removeClass("force-list"); 
+    }    
+}).trigger('resize');
