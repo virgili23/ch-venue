2fe5de639709909308fc4b9a115e52d89e575826
Virgilio <vbaez@bluecorona.com>
2022-07-04 17:37:42 -0400

    completed desktop version

--- assets/js/index.js
+++ assets/js/index.js
@@ -42,6 +42,19 @@ gridIcon.addEventListener("click", function() {
 //         }
 //     } 
 // }
+function openTab(event, socialName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("social-tab");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active-tab", "");
+    }
+    document.getElementById(socialName).style.display = "block";
+    event.currentTarget.className += " active-tab";
+  }
 
 $(window).bind("resize", function () {
 //    console.log($(this).width());
