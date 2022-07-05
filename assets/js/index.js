b5e42927c3831f265016e02f5431def0dbf7d3db
Virgilio <vbaez@bluecorona.com>
2022-07-04 22:51:07 -0400

    responsive update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,22 @@
+// Events Menu
 const listIcon = document.querySelector(".list-icon");
 const gridIcon = document.querySelector(".grid-icon");
 const artistsContainer = document.querySelector(".artists-container");
 
+// Hamburger Navigation
+const toggleBtn = document.getElementById("mobile-menu-toggle");
+const homeBody = document.getElementById("home");
+const faIcon = document.getElementById("fa-icon");
 
+
+// Toggle mobile menu
+toggleBtn.onclick = function() {
+    homeBody.classList.toggle("menu-active");
+    faIcon.classList.toggle("fa-xmark");
+};
+
+
+// For the Grid and List buttons
 listIcon.addEventListener("click", function() {
     artistsContainer.classList.add("list");
     gridIcon.classList.remove("active");
@@ -16,32 +30,7 @@ gridIcon.addEventListener("click", function() {
 });
 
 
-// function resize() {
-//     if ( ($(window).innerWidth() < 1199) && (!(artistsContainer.classList.contains(".list"))) ) {
-//         if (!gridIcon.classList.contains(".active")) {
-//             gridIcon.classList.remove("active");
-//             listIcon.classList.add("active");
-//         }
-//         artistsContainer.classList.add("list");
-
-//     } else if (($(window).innerWidth() >= 1200) && (!(artistsContainer.classList.contains(".list")))) {
-//         if (!listIcon.classList.contains(".active")) {
-//             listIcon.classList.remove("active");
-//             gridIcon.classList.add("active");
-//         }
-//         artistsContainer.classList.remove("list");
-//     }
-// }
-
-// function resize() {
-//     if ( ($(window).innerWidth() < 1200) ) {
-//         if (!artistsContainer.classList.contains("force-list")) {
-//             artistsContainer.classList.add("force-list");
-//         } else {
-//             artistsContainer.classList.remove("force-list");
-//         }
-//     } 
-// }
+// For the social Media Tabs
 function openTab(event, socialName) {
     var i, x, tablinks;
     x = document.getElementsByClassName("social-tab");
@@ -56,11 +45,18 @@ function openTab(event, socialName) {
     event.currentTarget.className += " active-tab";
   }
 
+// Fire function to force List view on smaller screens while keeping previous configurations active
 $(window).bind("resize", function () {
 //    console.log($(this).width());
     if ($(this).width() < 1200) {
         $(".artists-container").addClass("force-list"); 
     } else {
         $(".artists-container").removeClass("force-list"); 
-    }    
+    } 
+    
+    // Also remove mobile navbar open if exited mobile
+    if ($(this).width() >= 1200) {
+        homeBody.classList.remove("menu-active");
+        faIcon.classList.remove("fa-xmark");
+    }
 }).trigger('resize');
